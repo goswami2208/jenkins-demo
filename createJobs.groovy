6030f8d5382fc75ddcdd90613e41e360783b2660
joy <goswami.joy@gmail.com>
2021-10-28 09:21:21 -0400

    Latest changes

--- createJobs.groovy
+++ createJobs.groovy
@@ -20,3 +20,18 @@ pipelineJob('theme-park-job') {
         }
     }
 }
+pipelineJob('theme-park-job-docker') {
+    definition {
+        cpsScm {
+            scm {
+                git {
+                    remote {
+                        url 'https://github.com/goswami2208/spring-boot-api-example.git'
+                    }
+                    branch 'master'
+                    scriptPath('Jenkinsfile-docker')
+                }
+            }
+        }
+    }
+}
